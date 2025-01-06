6f2370ae8b9c3f96fd70d7d6640200c6b696d03c
Vishal Thakur <157201590+vishalthakur0012@users.noreply.github.com>
2025-01-06 16:23:54 +0530

    dskjlflsdfsd

--- components/Contact_Form.jsx
+++ components/Contact_Form.jsx
@@ -32,7 +32,7 @@ const Contact_Form = () => {
     }
 
     try {
-      const response = await fetch('http://localhost:5000/send-email', {
+      const response = await fetch('https://astrologer-jgs0.onrender.com/send-email', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
