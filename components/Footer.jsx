eff582769cfe76e89fb4b4c4429d42d18707f8b6
Vishal Thakur <157201590+vishalthakur0012@users.noreply.github.com>
2025-01-07 15:26:48 +0530

    skdjs

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import Link from 'next/link';
-import {  FaPhone, FaEnvelope } from "react-icons/fa6";
-import { FaFacebookF, FaInstagram, FaTwitter } from "react-icons/fa";
+import { FaPhone, FaEnvelope } from "react-icons/fa6";
+import { FaFacebookF, FaInstagram } from "react-icons/fa";
+import { FaWhatsapp } from "react-icons/fa6";
+import { BiSolidPhoneCall } from "react-icons/bi";
 const Footer = () => {
     return (
         <>
@@ -29,14 +31,14 @@ const Footer = () => {
                             <div className="footer-box">
                                 <h3>Our Services</h3>
                                 <ul>
-                                    <li><Link href="/">Love Problem Solution</Link></li>
-                                    <li><Link href="/">One Side Love Solution</Link></li>
-                                    <li><Link href="/">Breakup Problem</Link></li>
-                                    <li><Link href="/">Ex Love Back</Link></li>
-                                    <li><Link href="/">Family Dispute</Link></li>
-                                    <li><Link href="/">Grah Kalesh</Link></li>
-                                    <li><Link href="/">Love Marriage</Link></li>
-                                    <li><Link href="/">Business Problem Solution</Link></li>
+                                    <li><Link href="/love-problem-solution">Love Problem Solution</Link></li>
+                                    <li><Link href="/one-side-love-solution">One Side Love Solution</Link></li>
+                                    <li><Link href="/breakup-problem">Breakup Problem</Link></li>
+                                    <li><Link href="/ex-love-back">Ex Love Back</Link></li>
+                                    <li><Link href="/family-dispute">Family Dispute</Link></li>
+                                    <li><Link href="/grah-kalesh">Grah Kalesh</Link></li>
+                                    <li><Link href="/love-marriage">Love Marriage</Link></li>
+                                    <li><Link href="/business-problem-solution">Business Problem Solution</Link></li>
                                 </ul>
                             </div>
                         </div>
@@ -44,7 +46,7 @@ const Footer = () => {
                             <div className="footer-box2">
                                 <h3>Get In Touch</h3>
                                 <ul>
-                                    
+
                                     <li>
                                         <Link href="tel:+917717292608">
                                             <span className="icon"><FaPhone /></span>
@@ -70,6 +72,23 @@ const Footer = () => {
                     </div>
                 </div>
             </footer>
+
+            <Link
+                href="https://api.whatsapp.com/send?phone=+917717292608&text=Hello%20there!%20I%20would%20like%20to%20know%20more%20about%20your%20services."
+                className="float"
+                target="_blank"
+            >
+                <FaWhatsapp />
+            </Link>
+
+            <Link
+                href="tel:+917717292608"
+                className="float2"
+                target="_blank"
+            >
+                <BiSolidPhoneCall />
+            </Link>
+
 
             <div className="copyright-area">
                 <div className="container">
